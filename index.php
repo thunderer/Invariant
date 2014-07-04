a5dc303bcaefe5795863ce200ddfff34c5ed9d1d
Tomasz Kowalczyk <tomasz@kowalczyk.cc>
2014-07-04 13:30:57 +0200

    [WIP] redesign

--- index.php
+++ index.php
@@ -1,8 +1,22 @@
 <?php get_header(); ?>
 
+<ul class="pages">
+    <?php wp_list_pages('title_li='); ?>
+    <li>
+        <form role="search" method="get" id="searchform" class="searchform" action="<?php bloginfo('url'); ?>">
+            <div>
+                <input type="text" value="<?php echo get_search_query(); ?>" name="s" id="s" placeholder="szukaj..." />
+            </div>
+        </form>
+    </li>
+</ul>
+
+</div>
+<div class="container-outer">
+
 <?php while(have_posts()): ?>
 <?php the_post(); ?>
-<div class="container-short">
+<div class="block">
     <div class="container-header">
         <a href="<?php the_permalink(); ?>"><?php the_title(); ?></a>
     </div>
@@ -18,6 +32,8 @@
         <div class="category"><?php the_category(); ?></div>
     </div>
 </div>
+</div>
+<div class="container-outer">
 <?php endwhile; ?>
 
 <div class="nav nav-prev"><?php next_posts_link('&laquo;'); ?></div>
