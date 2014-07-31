20926d904743d618734603dfa54126e77459c166
Tomasz Kowalczyk <tomasz@kowalczyk.cc>
2014-07-31 12:39:26 +0200

    simplified themes, nice new CSS blocks

--- index.php
+++ index.php
@@ -5,20 +5,18 @@
 <?php $post = get_post(); ?>
 <?php $imageUrl = wp_get_attachment_thumb_url(get_post_thumbnail_id($post->ID)); ?>
 <div class="block post">
-    <div class="post-header-image" style="background-image: url('<?php echo $imageUrl; ?>')">
-        &nbsp;
-    </div>
-    <div class="block-header">
+    <div class="block-header post-header-image" style="background-image: url('<?php echo $imageUrl; ?>')">
         <a class="post-header" href="<?php the_permalink(); ?>"><?php the_title(); ?></a>
     </div>
     <div class="block-content post-content">
-        <?php the_excerpt(); ?>
+        <?php is_home() ? the_excerpt() : the_content(); ?>
     </div>
-    <div class="post-info">
+    <div class="post-info" style="background-image: url('<?php echo $imageUrl; ?>')">
         <div class="category"><?php the_category(); ?></div>
         <div class="tags"><?php the_tags('', ', ', ''); ?></div>
     </div>
 </div>
+<?php if(!is_home()) { comments_template(); } ?>
 <?php endwhile; ?>
 
 <div class="block nav">
--- single.php
+++ single.php
@@ -1,25 +1,2 @@
-<?php get_header(); ?>
-
-<?php while(have_posts()): ?>
-<?php the_post(); ?>
-<?php $post = get_post(); ?>
-<?php $imageUrl = wp_get_attachment_thumb_url(get_post_thumbnail_id($post->ID)); ?>
-<div class="block post">
-    <div class="post-header-image" style="background-image: url('<?php echo $imageUrl; ?>')">
-        &nbsp;
-    </div>
-    <div class="block-header">
-        <a class="post-header" href="<?php the_permalink(); ?>"><?php the_title(); ?></a>
-    </div>
-    <div class="block-content post-content">
-        <?php the_content(); ?>
-    </div>
-    <div class="post-info">
-        <div class="category"><?php the_category(); ?></div>
-        <div class="tags"><?php the_tags('', ', ', ''); ?></div>
-    </div>
-</div>
-<?php comments_template(); ?>
-<?php endwhile; ?>
-
-<?php get_footer(); ?>
+<?php
+require(__DIR__.'/index.php');
